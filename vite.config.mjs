31fd40f5d8422d252763c655305b3b04ff7bf890
abearxiong <xiongxiao1012@outlook.com>
2025-01-03 00:48:49 +0800

    fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,10 +4,17 @@ import dayjs from 'dayjs';
 import path from 'path';
 
 const isDev = process.env.NODE_ENV === 'development';
+const isWebDev = process.env.WEB_DEV === 'true';
 const BUILD_TIME = dayjs().format('YYYY-MM-DD HH:mm:ss');
+let plugins = [];
+
+if (!isWebDev) {
+  // 在bolt的web开发环境下不需要ssl
+  plugins.push(basicSsl());
+}
 
 export default defineConfig({
-  plugins: [basicSsl()],
+  plugins: [],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
